6cfd16e9b2a8af9646492f78ddc7f0707ff55f7b
pouyalnx <pouyanbie@gmail.com>
2021-08-26 08:29:41 -0400

    new day

---
+++ bash/bash/gigasecond/gigasecond.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# The following comments should help you get started:
+# - Bash is flexible. You may use functions or write a "raw" script.
+#
+# - Complex code can be made easier to read by breaking it up
+#   into functions, however this is sometimes overkill in bash.
+#
+# - You can find links about good style and other resources
+#   for Bash in './README.md'. It came with this exercise.
+#
+#   Example:
+#   # other functions here
+#   # ...
+#   # ...
+#
+#   main () {
+#     # your main function code here
+#   }
+#
+#   # call main with all of the positional arguments
+#   main "$@"
+#
+# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+old=`date --date='1977-06-13' '+%s'`
+now=$((old + 1000000000))
+date --date="@$now" '+%Y-%m-%dT%I:%M:%S'
